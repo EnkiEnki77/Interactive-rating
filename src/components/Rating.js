8d716d2b9da82a0e083e8ace733ab1119bdacd83
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-04-03 23:46:15 -0700

    first commit

---
+++ src/components/Rating.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Rating = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #131518;
+
+`;
+
+export default Rating
